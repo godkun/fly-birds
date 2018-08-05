6b96df4e58a980e78c62eece1db1cdb3fcee956e
godkun <godkun666@126.com>
2018-08-05 23:09:19 +0800

    将各种变量存到缓存器中

--- Main.js
+++ Main.js
@@ -1,14 +1,22 @@
 import {ResourceLoader} from "./js/base/ResourceLoader.js";
+import {DataStore} from "./js/base/DataStore";
 
 export class Main {
   constructor() {
     this.canvas = document.getElementById('canvas')
     this.ctx = this.canvas.getContext('2d')
+    this.dataStore = DataStore.getInstace()
     const loader = ResourceLoader().create()
     loader.onLoaded(map => this.onResourceFirstLoaded(map))
   }
 
-  onResourceFirstLoaded() {
+  // 将各种变量存到缓存器类里面
+  onResourceFirstLoaded(map) {
+    // 需要长期保存的放在类的变量中
+    // 需要实时销毁的放在map中
+    this.dataStore.canvas = this.canvas
+    this.dataStore.ctx = this.ctx
+    this.dataStore.res = map
     this.init()
   }
 
