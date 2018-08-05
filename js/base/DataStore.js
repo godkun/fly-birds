e3bd5be46fe51c81c6ea87b89c6d598a52b669b2
godkun <godkun666@126.com>
2018-08-05 22:51:00 +0800

    完成 DataStore类 缓存器 方便我们在不同的类中访问和修改变量

---
+++ js/base/DataStore.js
@@ -0,0 +1,32 @@
+export class DataStore {
+  static getInstace() {
+    if (!DataStore.instacne) {
+      DataStore.instacne = new DataStore()
+    }
+    return DataStore.instacne
+  }
+
+  constructor() {
+    // 使用 map进行缓存
+    this.map = new Map()
+  }
+
+  put(key, value) {
+    // 如果value是类，返回值\
+    if (typeof value === 'function') {
+      value = new value()
+    }
+    this.map.set(key, value)
+    return this
+  }
+
+  get(key) {
+    return this.map.get(key)
+  }
+
+  destroy() {
+    for(let value of this.map.values()) {
+        value = null
+    }
+  }
+}
