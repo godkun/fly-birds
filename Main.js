1ab810726212b2d3f12058281e1b675de5642fd7
godkun <godkun666@126.com>
2018-08-05 22:43:29 +0800

    图片资源加载完成 执行缓存方法

--- Main.js
+++ Main.js
@@ -1,3 +1,19 @@
+import {ResourceLoader} from "./js/base/ResourceLoader.js";
+
 export class Main {
-  
+  constructor() {
+    this.canvas = document.getElementById('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    const loader = ResourceLoader().create()
+    loader.onLoaded(map => this.onResourceFirstLoaded(map))
+  }
+
+  onResourceFirstLoaded() {
+    this.init()
+  }
+
+  init() {
+
+  }
+
 }
